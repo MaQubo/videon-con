5e98b7278902f72da2c29710c74e952beb6ba209
MaQubo <150929048+MaQubo@users.noreply.github.com>
2026-05-23 22:56:53 -0400

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Videos con...",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
+}
